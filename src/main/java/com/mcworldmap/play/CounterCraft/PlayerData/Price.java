fa6501c9f2a4e072128b2e10267cc67f95329421
larperdoodle <larsbenedetto@gmail.com>
2015-06-03 22:33:38 -0700

    Upgraded weapon price system

---
+++ src/main/java/com/mcworldmap/play/CounterCraft/PlayerData/Price.java
@@ -0,0 +1,12 @@
+package com.mcworldmap.play.CounterCraft.PlayerData;
+
+import java.util.HashMap;
+
+public class Price
+{
+	public HashMap<String,Integer> price = new HashMap<String,Integer>();
+	public Price(){
+		price.put("kevlar", 650);
+		price.put("helmet", 350);
+	}
+}
--- src/main/java/com/mcworldmap/play/CounterCraft/commands/CmdBuy.java
+++ src/main/java/com/mcworldmap/play/CounterCraft/commands/CmdBuy.java
@@ -2,17 +2,18 @@ package com.mcworldmap.play.CounterCraft.commands;
 
 import com.mcworldmap.play.CounterCraft.CounterCraft;
 import com.mcworldmap.play.CounterCraft.PlayerData.Person;
+import com.mcworldmap.play.CounterCraft.PlayerData.Price;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.w3c.dom.css.Counter;
 
 public class CmdBuy implements CommandExecutor
 {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String s, String[] args)
 	{
+		Price item = new Price();
 		if (!(sender instanceof Player))
 			sender.sendMessage("This command can only be run by a player.");
 		if (sender.hasPermission("CounterCraft.start"))
@@ -25,16 +26,10 @@ public class CmdBuy implements CommandExecutor
 			{
 				Person p = CounterCraft.team.findPerson((Player) sender);
 				sender.sendMessage("You have $" + p.getMoney());
-				if (args[0].equalsIgnoreCase("helmet"))
-					if (p.getMoney() >= 350)
-						p.setMoney(p.getMoney() - 350);
-					else
-						sender.sendMessage("You do not have enough money");
-				if (args[0].equalsIgnoreCase("kevlar"))
-					if (p.getMoney() >= 650)
-						p.setMoney(p.getMoney() - 650);
-					else
-						sender.sendMessage("You do not have enough money");
+				if (p.getMoney() >= item.price.get(args[0])){
+					p.setMoney(p.getMoney() - item.price.get(args[0]));
+				}
+
 			}
 		}
 		return false;

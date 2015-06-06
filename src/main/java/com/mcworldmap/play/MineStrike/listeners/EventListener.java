6b3a05f6fdd1fe131b01b3c07e1ee5014f9c3d7b
styyxxyx <mersennetwizter@gmail.com>
2015-06-06 15:28:27 -0700

    fix dat shit yo

--- src/main/java/com/mcworldmap/play/MineStrike/listeners/EventListener.java
+++ src/main/java/com/mcworldmap/play/MineStrike/listeners/EventListener.java
@@ -109,12 +109,21 @@ public class EventListener implements Listener
 				List<Entity> nearbyEntities = pot.getNearbyEntities(20, 20, 20);
 				nearbyEntities.stream().filter(e -> e instanceof Player).forEach(e -> ((Player) e).playSound(loc, Sound.FIRE, 2, 1));
 				nearbyEntities.stream().filter(e -> e instanceof Player).forEach(e -> ((Player) e).playSound(loc, Sound.FIRE_IGNITE, 2, 1));
+//				for (int x = 0; x < 5; x++)
+//				{
+//					for(int z = 0; z < 5; z++)
+//					{
+//						if (loc.getBlock().getType().equals(Material.AIR))
+//							loc.add(x, 0, z).getBlock().setType(Material.FIRE);
+//					}
+//				}
 				for (int x = 0; x < 5; x++)
 				{
 					for(int z = 0; z < 5; z++)
 					{
 						if (loc.getBlock().getType().equals(Material.AIR))
-							loc.add(x, 0, z).getBlock().setType(Material.FIRE);
+//							loc.add(x, 0, z).getBlock().setType(Material.FIRE);
+							loc.getWorld().getBlockAt(x, (int)loc.getY(), z).setType(Material.FIRE);
 					}
 				}
 			}

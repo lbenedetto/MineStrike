d8f15a9fb9376724a88cfbbf6304b2f2658327d9
styyxxyx <mersennetwizter@gmail.com>
2015-06-08 18:41:14 -0700

    This should force the player to respawn.

--- src/main/java/com/mcworldmap/play/MineStrike/Util/Util.java
+++ src/main/java/com/mcworldmap/play/MineStrike/Util/Util.java
@@ -4,11 +4,14 @@ import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerListHeaderFooter;
 import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
 import net.minecraft.server.v1_8_R3.PlayerConnection;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class Util
 {
@@ -21,6 +24,29 @@ public class Util
 	public static void sendSubtitle(Player player, Integer fadeIn, Integer stay, Integer fadeOut, String message) {
 		sendTitle(player, fadeIn, stay, fadeOut, null, message);
 	}
+
+	public static void autoRespawnPlayer(Player who) throws ReflectiveOperationException {
+		String bukkitversion = Bukkit.getServer().getClass()
+				.getPackage().getName().substring(23);
+		Class<?> cp = Class.forName("org.bukkit.craftbukkit."
+				+ bukkitversion + ".entity.CraftPlayer");
+		Class<?> clientcmd = Class.forName("net.minecraft.server."
+				+ bukkitversion + ".PacketPlayInClientCommand");
+		Class enumClientCMD = Class.forName("net.minecraft.server."
+				+ bukkitversion + ".EnumClientCommand");
+		Method handle = cp.getDeclaredMethod("getHandle", null);
+		Object entityPlayer = handle.invoke(who, null);
+		Constructor<?> packetconstr = clientcmd
+				.getDeclaredConstructor(enumClientCMD);
+		Enum<?> num = Enum.valueOf(enumClientCMD, "PERFORM_RESPAWN");
+		Object packet = packetconstr.newInstance(num);
+		Object playerconnection = entityPlayer.getClass()
+				.getDeclaredField("playerConnection").get(entityPlayer);
+		Method send = playerconnection.getClass().getDeclaredMethod("a",
+				clientcmd);
+		send.invoke(playerconnection, packet);
+	}
+
 
 	@Deprecated
 	public static void sendFullTitle(Player player, Integer fadeIn, Integer stay, Integer fadeOut, String title, String subtitle) {

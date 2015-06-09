e9162b8e312fa9d47dc4d69d9400afc9918d8b76
styyxxyx <mersennetwizter@gmail.com>
2015-06-08 18:01:01 -0700

    Added CmdGivemoney command.

---
+++ src/main/java/com/mcworldmap/play/MineStrike/commands/CmdGiveMoney.java
@@ -0,0 +1,26 @@
+package com.mcworldmap.play.MineStrike.commands;
+
+import com.mcworldmap.play.MineStrike.MineStrike;
+import com.mcworldmap.play.MineStrike.PlayerData.Person;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CmdGiveMoney implements CommandExecutor
+{
+
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		if(!(sender instanceof Player))
+			return false;
+
+		Person p = MineStrike.team.findPerson(Bukkit.getPlayer(args[0]));
+		p.setMoney(p.getMoney() + Integer.parseInt(args[1]));
+
+		return false;
+	}
+}

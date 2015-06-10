7ca1c0805b18442915a823357fcd4413f5702caf
larperdoodle <larsbenedetto@gmail.com>
2015-06-10 16:52:04 -0700

    Added loging for disabling shop

--- src/main/java/com/mcworldmap/play/MineStrike/Tasks/DisableShop.java
+++ src/main/java/com/mcworldmap/play/MineStrike/Tasks/DisableShop.java
@@ -2,11 +2,13 @@ package com.mcworldmap.play.MineStrike.Tasks;
 
 
 import com.mcworldmap.play.MineStrike.MineStrike;
+import org.bukkit.Bukkit;
 
 public class DisableShop implements Runnable
 {
 	@Override
 	public void run(){
+		Bukkit.getServer().getLogger().info("Shop disabled");
 		MineStrike.canBuy = false;
 	}
 }

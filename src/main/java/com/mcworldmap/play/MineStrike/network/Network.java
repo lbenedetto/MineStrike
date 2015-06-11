aa48a2b5aa0a9b4635bbc00a05293d06e5c53fb9
Styyxxyx <mersennetwizter@gmail.com>
2015-06-11 13:37:55 -0700

    Added MySQL support

--- src/main/java/com/mcworldmap/play/MineStrike/network/Network.java
+++ src/main/java/com/mcworldmap/play/MineStrike/network/Network.java
@@ -1,13 +1,58 @@
 package com.mcworldmap.play.MineStrike.network;
 
-public class Network {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class Network
+{
+    private Connection c;
+    String ip, database, password, username;
+    String url;
 
-
-    public Network()
+    public Network getInstance()
     {
-
+        return this;
     }
 
+    public Network(String ip, String database, String username, String password)
+    {
+        this.ip = ip;
+        this.database = database;
+        this.password = password;
+        this.username = username;
+        this.url = "jdbc:mysql://" + ip + ":3306/" + database;
+    }
 
+    public void init()
+    {
+        String query = "CREATE TABLE IF NOT EXISTS playerdata (username VARCHAR(256), kills VARCHAR(256), deaths VARCHAR(256), wins VARCHAR(256), losses VARCHAR(256)";
+
+        try
+        {
+            Statement s = c.createStatement();
+            s.executeUpdate(query);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void connect() {
+        try
+        {
+            c = DriverManager.getConnection(url, username, password);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection()
+    {
+        return c;
+    }
 }

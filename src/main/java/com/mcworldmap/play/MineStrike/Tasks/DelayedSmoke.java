71f2563a22e93784f418a04eae774275d7c339b2
larperdoodle <larsbenedetto@gmail.com>
2015-11-27 15:36:12 -0800

    fixed type in for loop

--- src/main/java/com/mcworldmap/play/MineStrike/Tasks/DelayedSmoke.java
+++ src/main/java/com/mcworldmap/play/MineStrike/Tasks/DelayedSmoke.java
@@ -33,7 +33,7 @@ public class DelayedSmoke implements Runnable {
         Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),
                 "particle hugeexplosion " + x + " " + y + " " + z + " 1 1 1 0 10 force");
         for (int ix = -5; ix < 5; ix++)
-            for (int iz = -5; iz < 5; ix++)
+            for (int iz = -5; iz < 5; iz++)
                 if (Math.abs(iz) + Math.abs(ix) <= 5)
                     for (int iy = 0; iy < 4; iy++) {
                         int checkX = x + ix;

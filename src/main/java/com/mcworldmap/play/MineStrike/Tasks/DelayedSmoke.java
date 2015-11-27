99bbc7ff8ef8205792f7995a03eec9b7f6121f69
larperdoodle <larsbenedetto@gmail.com>
2015-11-27 14:44:10 -0800

    Smoke grenades ready for testing, still need to be looped.

--- src/main/java/com/mcworldmap/play/MineStrike/Tasks/DelayedSmoke.java
+++ src/main/java/com/mcworldmap/play/MineStrike/Tasks/DelayedSmoke.java
@@ -1,9 +1,10 @@
 package com.mcworldmap.play.MineStrike.Tasks;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 
-public class DelayedSmoke implements Runnable{
+public class DelayedSmoke implements Runnable {
     World world;
     Location loc;
 
@@ -20,6 +21,12 @@ public class DelayedSmoke implements Runnable{
      * Create a particle cloud at specified location
      */
     public void run() {
-
+        // /particle <name> <x> <y> <z> <xd> <yd> <zd> <speed> [count = 1] [mode = normal]
+        // Spawn a cloud of 10000 particles at x,y,z with radius of 2,2,2 and speed. Force all players to see them
+        int x = (int) loc.getX();
+        int y = (int) loc.getY();
+        int z = (int) loc.getZ();
+        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),
+                "particle hugeexplosion" + x + " " + y + " " + z + "2 2 2 0 10 force");
     }
 }

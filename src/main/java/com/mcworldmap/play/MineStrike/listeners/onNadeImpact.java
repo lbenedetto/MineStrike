a197b1895e563a46fd88dc8e8f73ddd8c19df6c3
larperdoodle <larsbenedetto@gmail.com>
2015-11-27 16:07:35 -0800

    Improved decoy grenade

--- src/main/java/com/mcworldmap/play/MineStrike/listeners/onNadeImpact.java
+++ src/main/java/com/mcworldmap/play/MineStrike/listeners/onNadeImpact.java
@@ -88,9 +88,12 @@ public class onNadeImpact implements Listener {
                 List<Entity> nearbyEntities = pot.getNearbyEntities(20, 20, 20);
                 nearbyEntities.stream().filter(e -> e instanceof Player).forEach(e -> ((Player) e).playSound(loc, Sound.ANVIL_LAND, 1, 1));
                 //random bow shoot sounds in the vicinity of where the grenade landed for about 30ish seconds
-                for(int t = 20; t < 600; t+=ThreadLocalRandom.current().nextInt(10, 30 + 1))
+                for (int t = 20; t < 600; t += ThreadLocalRandom.current().nextInt(10, 30 + 1)) {
                     Bukkit.getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("MineStrike"),
-                            new DelayedSound(w,loc, pot), t);
+                            new DelayedSound(w, loc, pot, Sound.SHOOT_ARROW), t);
+                    Bukkit.getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("MineStrike"),
+                            new DelayedSound(w, loc, pot, Sound.ARROW_HIT), t+ThreadLocalRandom.current().nextInt(10, 40 + 1));
+                }
             }
             //Smoke Grenade
             if (effect.getType().equals(PotionEffectType.SLOW)) {
@@ -110,7 +113,7 @@ public class onNadeImpact implements Listener {
                                     b.setType(Material.AIR);
                                 }
                             }
-                for (int t = 20; t < 380; t+=10)
+                for (int t = 20; t < 380; t += 10)
                     Bukkit.getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("MineStrike"),
                             new DelayedSmoke(w, loc), t);
             }
